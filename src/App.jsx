69628cb7b62046f4ba169da8ef8183ae9cff7890
impallab <pallab13nayagram@gmail.com>
2024-04-08 02:19:56 +0530

    initialized Header and Footer component, setup react-redux Provider, and worked on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from "react-redux";
 import './App.css'
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import { Header } from './components';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(null);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h2>Blog Project using Appwrite as a backend</h2>
-    </>
+  useEffect(()=>{
+    authService.getCurrentuser()
+      .then( (userData)=>{
+        if (userData) {
+          dispatch(login({userData}));
+        } else {
+          dispatch(logout({userData}))
+        }
+      })
+      .catch(error => {
+        // setError('Failed to fetch current user data.');
+        console.error('Error fetching current user:', error);
+      })
+      .finally(()=>setLoading(false))
+  },[])
+  return !loading ? (
+    <div>
+      <div>
+        <Header/>
+        <main>
+          <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : (
+    <div>Loading ...</div>
   )
 }
 
