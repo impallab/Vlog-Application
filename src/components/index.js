665835e8bf50e1ef6a5b04b0d309e76a79cdd3f8
impallab <pallab13nayagram@gmail.com>
2024-05-06 15:47:54 +0530

    postForm component created

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import Container from "postcss/lib/container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Input from "postcss/lib/input";
+import RTE from "./RTE";
 
-export  { Header, Footer, Container, Logo, LogoutBtn,Input }
+export  { Header, Footer, Container, Logo, LogoutBtn,Input,RTE }
